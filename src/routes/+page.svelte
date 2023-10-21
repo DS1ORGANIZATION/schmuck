1084c5067ffdf9f47c18986499a6fe07fabd79e6
DS1BILLION <141220311+DS1BILLION@users.noreply.github.com>
2023-10-21 23:39:14 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-asnbdh
-adjhbsdua
-snbajf
-sdbau
-
-```
-
-
-## System Info
